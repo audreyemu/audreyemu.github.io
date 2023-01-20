3ec45ef5e6743bc868960a99e8e0ca34d5a6560d
audreyemu <121738351+audreyemu@users.noreply.github.com>
2023-01-20 15:24:13 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+var string = navigator.userAgent
+
+if(string == 'BagelLover'){
+  console.log('flag{mMm_yUmmm3rS_b4ge1s}');
+}
+else {
+  console.log('Only a BagelLover is able to view the flag');
+}

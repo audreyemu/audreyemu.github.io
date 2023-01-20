500bb84aa93a20754bec278e1874eeec99475ddd
audreyemu <121738351+audreyemu@users.noreply.github.com>
2023-01-20 15:28:48 -0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-var string = navigator.userAgent
-
-if(string == 'BagelLover'){
-  console.log('flag{mMm_yUmmm3rS_b4ge1s}');
-}
-else {
-  console.log('Only a BagelLover is able to view the flag');
-}

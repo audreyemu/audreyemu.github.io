6791cc113d6085e1534c1967e94d2681a9ea996e
audreyemu <121738351+audreyemu@users.noreply.github.com>
2023-01-20 16:51:31 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,15 +4,28 @@ const express = require("express");
 // create express app
 const app = express();
 
+// define port
+const port = 3000;
+
 // server static files
 app.use('/static', express.static("public"));
+
+// define route
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 // define challenge route
 app.get("/challenge/:word", (req, res) => {
   const word = req.params.word;
   if(word === "flag"){
-    res.send("flag{mMm_yUmmm3rS_b4ge1s}");
+    res.send("flag{flag}");
   } else {
     res.send("Not the flag");
   }
+});
+
+// listen for requests
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
 });
